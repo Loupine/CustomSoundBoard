2797f27777b8ba6801b7bfd4fc10e6c1cf857c13
Andrew Everage-Scheible <andreweveragescheible@gmail.com>
2023-09-27 00:27:05 -0400

    Rename Controller to MenuController

--- src/main/java/eve/sch/customsoundboard/MenuController.java
+++ src/main/java/eve/sch/customsoundboard/MenuController.java
@@ -1,14 +1,12 @@
 package eve.sch.customsoundboard;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
-public class Controller {
+public class MenuController {
     public Slider columnSlider;
     public Slider rowSlider;
     public Label columnLabel;
@@ -16,29 +14,29 @@ public class Controller {
 
     @FXML
 
-    public void onNewBoardButtonPress(ActionEvent actionEvent) throws IOException {
+    public void onNewBoardButtonPress() throws IOException {
         System.out.println("new board pressed");
         Application.loadBoardCreationMenu();
     }
 
 
-    public void onLoadBoardButtonPress(ActionEvent actionEvent) {
+    public void onLoadBoardButtonPress() {
         System.out.println("load board pressed");
     }
 
 
-    public void onUpdateColumnCount(MouseEvent mouseEvent) {
+    public void onUpdateColumnCount() {
         int number = (int) columnSlider.getValue();
         columnLabel.setText(number + " Column(s)");
     }
 
 
-    public void onUpdateRowCount(MouseEvent mouseEvent) {
+    public void onUpdateRowCount() {
         int number = (int) rowSlider.getValue();
         rowLabel.setText(number + " Row(s)");
     }
 
 
-    public void onCreateBoardButtonPress(ActionEvent actionEvent) {
+    public void onCreateBoardButtonPress() {
     }
 }
